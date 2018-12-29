21a5e78704c466a3dc719389e50804c196733f1c
mo no <mono1981163@gmail.com>
2018-12-29 15:03:23 +0100

    update xUnitTest

--- ForceFeedback/xUnitTest/ConfigPathTest.cs
+++ ForceFeedback/xUnitTest/ConfigPathTest.cs
@@ -10,10 +10,12 @@ namespace xUnitTest
     public class ConfigPathTest
     {
 		[Theory]
-		[InlineData("I:\\Work\\49_c# algorithm\\Source\\ThreeProb\\ThreeProb\\HashIterator.cs")]
+		//[InlineData("I:\\Work\\49_c# algorithm\\Source\\ThreeProb\\ThreeProb\\HashIterator.cs")]
+		[InlineData("C:\\projects\\forcefeedbackprogramming\\ForceFeedback\\ForceFeedback.Rules\\Configuration\\Global.cs")]
 		public void FindProjectOrSolutionTest(string path)
 		{
-			Assert.Equal("I:\\Work\\49_c# algorithm\\Source\\ThreeProb\\ThreeProb", ForceFeedback.Rules.Configuration.Global.GetProjectOrSolutionPath(path));
+			//Assert.Equal("I:\\Work\\49_c# algorithm\\Source\\ThreeProb\\ThreeProb", ForceFeedback.Rules.Configuration.Global.GetProjectOrSolutionPath(path));
+			Assert.Equal("C:\\projects\\forcefeedbackprogramming\\ForceFeedback\\ForceFeedback.Rules", ForceFeedback.Rules.Configuration.Global.GetProjectOrSolutionPath(path));
 		}
 	}
 }
