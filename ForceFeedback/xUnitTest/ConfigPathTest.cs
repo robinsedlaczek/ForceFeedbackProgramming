6c0acc7a74179b4a5af038430c9b287d76d6331b
mo no <mono1981163@gmail.com>
2019-01-07 18:24:07 +0100

    update test

--- ForceFeedback/xUnitTest/ConfigPathTest.cs
+++ ForceFeedback/xUnitTest/ConfigPathTest.cs
@@ -4,18 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
+using ForceFeedback.Rules.Configuration;
+using System.IO;
 
 namespace xUnitTest
 {
-    public class ConfigPathTest
+    public class FindConfigTest
     {
-		[Theory]
-		//[InlineData("I:\\Work\\49_c# algorithm\\Source\\ThreeProb\\ThreeProb\\HashIterator.cs")]
-		[InlineData("C:\\projects\\forcefeedbackprogramming\\ForceFeedback\\ForceFeedback.Rules\\Configuration\\Global.cs")]
-		public void FindProjectOrSolutionTest(string path)
+		private readonly ITestOutputHelper output;
+
+		public FindConfigTest(ITestOutputHelper output)
 		{
-			//Assert.Equal("I:\\Work\\49_c# algorithm\\Source\\ThreeProb\\ThreeProb", ForceFeedback.Rules.Configuration.Global.GetProjectOrSolutionPath(path));
-			Assert.Equal("C:\\projects\\forcefeedbackprogramming\\ForceFeedback\\ForceFeedback.Rules", ForceFeedback.Rules.Configuration.Global.GetProjectOrSolutionPath(path));
+			this.output = output;
+		}
+
+		[Theory]
+		[InlineData(@"..\..\..\..\Test\NoConfigTest")]
+		public void ConfigFileFindTest(string path)
+		{
+			// no config file is found for the solution
+			string configPath = Path.Combine(Environment.CurrentDirectory, path, Global.CONFIG_FILE_NAME);
+			bool isExist = File.Exists(configPath);
+			output.WriteLine("Config file for the solution path - {0} is {1}", configPath, isExist);
+
+			// no config file is found for the project
+			configPath = Path.Combine(Environment.CurrentDirectory, path, @"NoConfigTest", Global.CONFIG_FILE_NAME);
+			isExist = File.Exists(configPath);
+			output.WriteLine("Config file for the project path - {0} is {1}", configPath, isExist);
+
+			// no config file is found for global
+			configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), @"ForceFeedbackProgramming" + @"\" + Global.CONFIG_FILE_NAME);
+			isExist = File.Exists(configPath);
+			output.WriteLine("Config file for the global path - {0} is {1}", configPath, isExist);
 		}
 	}
 }
